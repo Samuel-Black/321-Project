2b8a3c8ca5e5dbd5056868170dce459228ec4ba2
Samuel-Black <58094651+Samuel-Black@users.noreply.github.com>
2021-03-31 00:56:21 +1100

    Added request-url file to avoid duplication

--- src/components/Request-URL.js
+++ src/components/Request-URL.js
@@ -1,7 +1,7 @@
 
-export const GetPlayersURL = 'http://ec2-107-23-136-97.compute-1.amazonaws.com:3001/api/getplayers'
-export const CreatePlayerURL = 'http://ec2-107-23-136-97.compute-1.amazonaws.com:3001/api/createplayer'
-export const CreateAttemptURL = 'http://ec2-107-23-136-97.compute-1.amazonaws.com:3001/api/createattempt'
-export const GetTotalProgressURL = 'http://ec2-107-23-136-97.compute-1.amazonaws.com:3001/api/gettotalprogress'
-export const GetLevelProgressURL = 'http://ec2-107-23-136-97.compute-1.amazonaws.com:3001/api/getlevelprogress'
+export const GetPlayersURL = 'http://localhost:3001/api/getplayers'
+export const CreatePlayerURL = 'http://localhost:3001/api/createplayer'
+export const CreateAttemptURL = 'http://localhost:3001/api/createattempt'
+export const GetTotalProgressURL = 'http://localhost:3001/api/gettotalprogress'
+export const GetLevelProgressURL = 'http://localhost:3001/api/getlevelprogress'
 

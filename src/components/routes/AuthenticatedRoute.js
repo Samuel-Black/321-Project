b38d06d4a3baf67be2953b62ebe7e6482d79d1c6
Samuel-Black <58094651+Samuel-Black@users.noreply.github.com>
2021-03-30 22:23:57 +1100

    fixes for amplify online config

--- src/components/routes/AuthenticatedRoute.js
+++ src/components/routes/AuthenticatedRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Route, Navigate, useNavigate } from 'react-router-dom';
 import { useAuthUser, useAuthPlayer } from '../../libs';
 
 //&& !Boolean(userDetails.token === getCurrentToken())
@@ -7,16 +7,16 @@ import { useAuthUser, useAuthPlayer } from '../../libs';
 const Container = ({Component, isPrivate, requiresPlayer, ...props}) => {
     const userData = useAuthUser()
     const player = useAuthPlayer()
-    console.log(player)
+    const navigate = useNavigate()
     
     if(isPrivate && userData === false) { // If attempting to access private routes while not logged in/authenticated
-      return <Navigate to={'/Login'} replace={true} />
+      return <Navigate to={'../../Login'} replace={true} />
     }
     if(!isPrivate && userData !== false) { // If attempting to access Sign Up or Log In page while currently logged in/authenticated
-      return <Navigate to={'../'} replace={true} />
+      return <Navigate to={'../../'} replace={true} />
     }
     if(requiresPlayer === true && player.player === false) { // If attempting to access Application while logged in/authenticated but without selecting their player account
-      return <Navigate to={'../'} replace={true} />
+      return <Navigate to={'../../'} replace={true} />
     }
     return <Component {...props} />
   }

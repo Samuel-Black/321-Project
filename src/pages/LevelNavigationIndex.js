08ecdb15f0dc63bc7dd56dcdb67b52b641ad3a43
Sam Black <sam.black604@gmail.com>
2021-04-28 16:06:01 +1000

    Home page, LevelNavigation page, and game page now mostly responsive from sams' testing.

--- src/pages/LevelNavigationIndex.js
+++ src/pages/LevelNavigationIndex.js
@@ -5,11 +5,13 @@ import { useAuthPlayer, useAuthUser } from '../libs'
 import Axios from 'axios'
 import { GetTotalProgressURL } from '../components/Request-URL'
 import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 import { RiUserFill } from 'react-icons/ri'
 import { TiTick } from 'react-icons/ti'
 import { BsArrowRightShort } from 'react-icons/bs'
 import LevelNavbar from '../components/LevelNavbar'
-import 'simplebar/dist/simplebar.min.css';
+import { getLocalPlayer } from '../components/localstorage/Local-Storage-Functions'
+import './LevelNavigationPage.scss'
 
 export default function LevelNavigationPage(props) {
     
@@ -41,7 +43,7 @@ export default function LevelNavigationPage(props) {
             })
         }
         else if(user === false) { // If not using an account and not logged in, get progress from local storage
-            let localPlayer = JSON.parse(localStorage.getItem(currentPlayer.player.NickName+'-34CUH8sLCXUZTA79X748'))
+            let localPlayer = getLocalPlayer(currentPlayer.player.NickName)
             let localPlayerProgress = localPlayer.Progress;
             let localProgress = []
 
@@ -65,7 +67,7 @@ export default function LevelNavigationPage(props) {
     return (
         <div id="Level-Nav-Background">
             <div className="container">
-                <div className="d-flex mb-3">
+                <div className="d-flex flex-wrap mb-3">
                     <div className="mr-auto">
                         <h1 id="Level-Navigation-Title">JumpStart</h1>
                     </div>
@@ -78,26 +80,28 @@ export default function LevelNavigationPage(props) {
                 
             </div>
             <div className="container mt-5">
-                <SimpleBar style={{ height: '70vh' }} autoHide={false}>
+                <SimpleBar style={{ height: '60vh' }} autoHide={false}>
                     <div id="Level-Nav">
-                        {Levels.map((level,i) => {
-                            return (    
-                                <Link to={level.to} key={i}>
-                                    <div className={`d-inline-flex justify-content-center align-items-center mr-4 mb-5 Game-Container ${getSkillProgress(level.name) >= level.numLevels ? 'green' : 'orange' }`}>
-                                        <div className="nav-item" id={"Game-"+level.id}>
-                                            <div className="d-flex justify-content-end">
-                                                <div className="d-inline-flex nav-item-svg">
-                                                    {getSkillProgress(level.name) >= level.numLevels ? <TiTick size={40} /> : <BsArrowRightShort size={40} /> }
+                        <div className= "d-flex flex-wrap justify-content-around">
+                            {Levels.map((level,i) => {
+                                return (    
+                                    <Link to={level.to} key={i}>
+                                        <div className={`d-inline-flex mb-5 Game-Container ${getSkillProgress(level.name) >= level.numLevels ? 'green' : 'orange' }`}>
+                                            <div className="nav-item" id={"Game-"+level.id}>
+                                                <div className="d-flex justify-content-end mr-2">
+                                                    <div className="d-flex nav-item-svg">
+                                                        {getSkillProgress(level.name) >= level.numLevels ? <TiTick size={40} /> : <BsArrowRightShort size={40} /> }
+                                                    </div>
                                                 </div>
+                                                <div className="d-flex justify-content-center"><img src={level.monster} /></div>
+                                                <div className="d-flex justify-content-center level-name">{level.name}</div>
+                                                <div className="d-flex justify-content-center level-progress">{`${getSkillProgress(level.name)} of ${level.numLevels} completed`}</div>
                                             </div>
-                                            <div className="d-flex justify-content-center"><img src={level.monster} /></div>
-                                            <div className="d-flex justify-content-center level-name">{level.name}</div>
-                                            <div className="d-flex justify-content-center level-progress">{`${getSkillProgress(level.name)} of ${level.numLevels} completed`}</div>
                                         </div>
-                                    </div>
-                                </Link>
-                            );
-                        })}
+                                    </Link>
+                                );
+                            })}
+                        </div>
                     </div>
                 </SimpleBar>
             </div>
--- src/pages/games/CardsGame.js
+++ src/pages/games/CardsGame.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { shuffleArray } from '../../components/images/Image-Functions'
+import SimpleBar from 'simplebar-react';
 import './CardsGame.scss'
 
 export default function CardsGame(props) {
@@ -53,25 +54,70 @@ export default function CardsGame(props) {
         }
     }
 
+    // Hook
+    function useWindowSize() {
+        // Initialize state with undefined width/height so server and client renders match
+        // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+        const [windowSize, setWindowSize] = useState({
+            width: undefined,
+            height: undefined,
+        });
+
+        useEffect(() => {
+            // Handler to call on window resize
+            function handleResize() {
+            // Set window width/height to state
+                setWindowSize({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+            }
+            
+            // Add event listener
+            window.addEventListener("resize", handleResize);
+            // Call handler right away so state gets updated with initial window size
+            handleResize();
+            // Remove event listener on cleanup
+            return () => window.removeEventListener("resize", handleResize);
+        }, []); // Empty array ensures that effect is only run on mount
+
+        return windowSize;
+    }
+
+    function SetRowJustification(width) { // cards are cut off by the simplebar component when statically defined as centered, this is a solution
+        if( (width < 1000 && currentCards.length === 3) || (width < 1668 && currentCards.length === 5) || (currentCards.length > 5) ) {
+            return '';
+        } else
+            return 'justify-content-center';
+    }
+
+    console.log(SetRowJustification(useWindowSize().width))
+
     return (
-        <>
-            <div className="container-fluid">
-                <div className="d-flex align-items-center justify-content-center">
-                    {difficulty <= levels &&
-                    <>
-                        {currentCards.map((image, i) => {
-                            return(
-                                <div key={i} className="d-inline-flex card-option mr-2">
-                                    <a onClick={() => WinCondition(image.correct)} >
-                                        <img src={image.default} />
-                                    </a>
-                                </div>
-                            )
-                        })}
-                    </>
-                    }
-                </div>
+        <div className="container-fluid">
+            <div className="row justify-content-center">
+                <SimpleBar style={{ width: '70vw' }} autoHide={false}>
+                    <div className="container-fluid">
+                        <div className={`row ${SetRowJustification(useWindowSize().width)}`}>
+                            <div className="d-flex">
+                                {difficulty <= levels &&
+                                    <>
+                                        {currentCards.map((image, i) => {
+                                            return(
+                                                <div key={i} className="card-option mr-2">
+                                                    <a onClick={() => WinCondition(image.correct)} >
+                                                        <img src={image.default} />
+                                                    </a>
+                                                </div>
+                                            )
+                                        })}
+                                    </>
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </SimpleBar>
             </div>
-        </>
+        </div>
     )
 }

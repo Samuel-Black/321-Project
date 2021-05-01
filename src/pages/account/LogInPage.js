7f085a6ad353588bd8952c242c337d7d2e0a61b2
atglenio <atglenio@gmail.com>
2021-05-01 16:01:38 +1000

    add responsiveness to login page

--- src/pages/account/LogInPage.js
+++ src/pages/account/LogInPage.js
@@ -27,8 +27,53 @@ export default function LoginPage() {
     }
 
     return (
-        <div id="Login-Background">
-            <div className="container">
+        <div id="Base-container" className="container">
+          <div id="Login-Content-Container" className="container">
+              <div id="Title-Row" className="row justify-content-center">
+                <h1>JumpStart</h1>
+              </div>
+              <div id="Login-Content-Row" className="row">
+                <div className="container">
+                  <div className="row justify-content-md-center">
+                    <div id="Login-Content" className="d-inline-flex flex-column align-items-center justify-content-center">
+                          <div className="row justify-content-md-center">
+                            <h2>Sign In</h2>
+                          </div>
+                        <span>Don't have an account?&nbsp;
+                            <Link to='../Signup'>
+                                <a>Sign Up Here</a>
+                            </Link>
+                        </span>
+                        <form>     
+                            <div className="form-group">
+                              <label htmlFor="email" className="align-self-center">Email</label>
+                              <input type="text" id='email' className="form-control" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} disabled={loading} />
+                            </div>
+                            <div className="form-group">
+                              <label htmlFor="password" className="align-self-center">Password</label>
+                              <input type="password" id='password' className="form-control" placeholder="password" value={password} minLength={8} onChange={(e) => setPassword(e.target.value)} disabled={loading} />
+                            </div>
+                            <div className="d-flex justify-content-end">{errorMessage ? <p>{errorMessage}</p> : null}</div>
+                            <span>
+                                <Link to='./'>
+                                    <a>Forgot your password?</a>
+                                </Link>
+                            </span>
+                            <div className="d-flex justify-content-end">
+                                {loading === true && <Oval />}<button id="Login-Button" onClick={handleLogin} disabled={loading}>login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+}
+/*
+
+  <div className="container">
                 <div id="Title-Row" className="row">
                     <div className="container">
                         <div id="Login-Title" class="row justify-content-md-center">
@@ -80,6 +125,5 @@ export default function LoginPage() {
                     </div>
                 </div>
             </div>
-        </div>
-    )
-}
+            */
+

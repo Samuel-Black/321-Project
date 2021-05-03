4cf85363e859305cbec5365996c0f74989c57cf9
atglenio <atglenio@gmail.com>
2021-05-03 16:47:25 +1000

    responsive

--- src/pages/account/LogInPage.js
+++ src/pages/account/LogInPage.js
@@ -27,6 +27,7 @@ export default function LoginPage() {
     }
 
     return (
+      <div className="app">
         <div id="Base-container" className="container">
           <div id="Login-Content-Container" className="container">
               <div id="Title-Row" className="row justify-content-center">
@@ -69,61 +70,7 @@ export default function LoginPage() {
             </div>
           </div>
         </div>
+      </div>
     )
 }
-/*
-
-  <div className="container">
-                <div id="Title-Row" className="row">
-                    <div className="container">
-                        <div id="Login-Title" class="row justify-content-md-center">
-                            <h1>JumpStart</h1>
-                        </div>
-                    </div>
-                </div>
-                <div id="Login-Content-Row" className="row">
-                    <div className="container">
-                        <div className="row justify-content-md-center">
-                            <h2>Sign In</h2>
-                        </div>
-                        <div className="row justify-content-md-center">
-                            <div id="Login-Content" className="d-inline-flex flex-column align-items-center justify-content-center">
-                                <span>Don't have an account?&nbsp;
-                                    <Link to='../Signup'>
-                                        <a>Sign Up Here</a>
-                                    </Link>
-                                </span>
-                                <form className="mt-3">
-                                    <div className="d-flex">
-                                        <label htmlFor="email" className="align-self-center">Email</label>
-                                    </div>
-                                    <div className="d-flex">
-                                        <div className="form-group">
-                                            <input type="text" id='email' className="form-control-lg" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} disabled={loading} />
-                                        </div>
-                                    </div>
-                                    <div className="d-flex">
-                                        <label htmlFor="password" className="align-self-center">Password</label>
-                                    </div>
-                                    <div className="d-flex">
-                                        <div className="form-group">
-                                            <input type="password" id='password' className="form-control-lg" placeholder="password" value={password} minLength={8} onChange={(e) => setPassword(e.target.value)} disabled={loading} />
-                                        </div>
-                                    </div>
-                                    <div className="d-flex justify-content-end">{errorMessage ? <p>{errorMessage}</p> : null}</div>
-                                    <span>
-                                        <Link to='./'>
-                                            <a>Forgot your password?</a>
-                                        </Link>
-                                    </span>
-                                    <div className="d-flex justify-content-end">
-                                        {loading === true && <Oval />}<button id="Login-Button" onClick={handleLogin} disabled={loading}>login</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            */
 
